b382e43f3fa8726f680909275161205925efd970
jandcreative <javi.diaz@jandcreative.com>
2025-02-26 16:17:20 +0000

    footer - php

--- footer.php
+++ footer.php
@@ -1,13 +1,39 @@
 <footer>
-	<div class="footer-nav container">
+	<div class="footer-nav">
 
 	<div class="column">
 			<?php the_field('text_social_media', 'options'); ?>
 	</div>
+
+	<div class="column">
+	<h4>Ver también:</h4>
+		<?php wp_nav_menu(
+			array(
+				'theme_location' => 'secondary',
+				'container' => 'nav',
+				'container_id' => 'enlace',
+			)
+		);
+		?>
+	</div>
+
+	<div class="column">
+	<h4>Legal:</h4>
+		<?php wp_nav_menu(
+			array(
+				'theme_location' => 'legal',
+				'container' => 'nav',
+				'container_id' => 'enlace',
+			)
+		);
+		?>
+	</div>
+
 	<div class="column">
 		<?php the_field('html_social_media', 'options'); ?>
 
 	</div>
+
 	</div>
 
 </footer>
