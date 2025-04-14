6c4b073a7cd51a7052dda9404a9ff3986273412c
jandcreative <javi.diaz@jandcreative.com>
2025-04-14 12:45:48 +0200

    add contact page

---
+++ page-contact.php
@@ -0,0 +1,19 @@
+<?php
+/* Template Name: Page Contact */
+/* Template Post Type: post, page, product */
+?>
+
+<?php get_header(); ?>
+
+	<div id="page-wrapper">
+
+		<?php the_content(); ?>
+
+	</div>
+
+	<?php get_footer(); ?>
+</body>
+</html>
+
+
+
